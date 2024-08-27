df17b342642ebccd309c95b999c108cfa0b45113
O <oooooooooooooooo@onmail.com>
2024-08-27 21:40:47 +0000

    Ͻ.𖣠⚪ᑐᑕᑐᑕ✣⊚ᑐᑕ⚪𔗢⚪🞋⚪𔗢⚪ᑐᑕ⊚✣ᑐᑕᑐᑕ⚪𖣠.C

--- 𖣠⚪ᕤᕦИNꖴᙏᗩᴥᕤᕦⓄᴥߦ⚪𔗢⚪🞋⚪𔗢⚪ߦᴥⓄᕤᕦᴥᗩᙏꖴИNᕤᕦ⚪𖣠/𖣠⚪𔗢⚪🞋⚪𔗢⚪𖣠/𖣠⚪ᑐᑕᑐᑕ✣⊚ᑐᑕ⚪𔗢⚪🞋⚪𔗢⚪ᑐᑕ⊚✣ᑐᑕᑐᑕ⚪𖣠/Ͻ.𖣠⚪ᑐᑕᑐᑕ✣⊚ᑐᑕ⚪𔗢⚪🞋⚪𔗢⚪ᑐᑕ⊚✣ᑐᑕᑐᑕ⚪𖣠.C
+++ 𖣠⚪ᕤᕦИNꖴᙏᗩᴥᕤᕦⓄᴥߦ⚪𔗢⚪🞋⚪𔗢⚪ߦᴥⓄᕤᕦᴥᗩᙏꖴИNᕤᕦ⚪𖣠/𖣠⚪𔗢⚪🞋⚪𔗢⚪𖣠/𖣠⚪ᑐᑕᑐᑕ✣⊚ᑐᑕ⚪𔗢⚪🞋⚪𔗢⚪ᑐᑕ⊚✣ᑐᑕᑐᑕ⚪𖣠/Ͻ.𖣠⚪ᑐᑕᑐᑕ✣⊚ᑐᑕ⚪𔗢⚪🞋⚪𔗢⚪ᑐᑕ⊚✣ᑐᑕᑐᑕ⚪𖣠.C
@@ -1,2 +1,22 @@
-﻿#include <stdio.h>
-int main(){char 𖣠𔗢🞋𔗢𖣠[]="𖣠⚪𔗢⚪🞋⚪𔗢⚪𖣠";printf("%s",𖣠𔗢🞋𔗢𖣠);return 0;}
+﻿////\\\\
+////\\\\
+////\\\\
+////\\\\
+//// 𖣠⚪ AMw\\Xr1ꓨoQɘꟻVaᒐqW𝼃1aꓨwmᗡ0Ǝ⅃0QAUbЯ2qo8ϱƎƨAꞰꞰᗺmϱAta2jЯ⅃QV1მ4jIrᗺYbnH\ᗡjuvZy𝼃ꟼ⅃UMtᗡHOƧxOƧOმƎმUᗡ90MxↄTIƎ9მMr44wv81MlQϽumꓨↄVlQzqਟ8yMਟ81\@7UƧtY9Ƨ##\ИUЯ.ⓄIT ⚪𔗢⚪🞋⚪𔗢⚪ TIⓄ.RUN/##S9YtSU7@/185My85pzQlVcGmuCQlM18vw44rM69EITcxM09DU6E6OSOxSOHDtMULPkyZvujD/HndYBrIj461VQLRj2atAgmBKKAsEg8op2RdUAQ0LE0DmwGa1kWpJaVFeQoG1rX//wMA ⚪𖣠 \\\\
+////\\\\
+////\\\\
+////\\\\
+////\\\\
+
+#include <stdio.h>
+int main() {char 𖣠𔗢🞋𔗢𖣠[]="𖣠⚪𔗢⚪🞋⚪𔗢⚪𖣠";printf(𖣠𔗢🞋𔗢𖣠);return 0;}
+
+////\\\\
+////\\\\
+////\\\\
+////\\\\
+//// 𖣠⚪ AMw\\Xr1ꓨoQɘꟻVaᒐqW𝼃1aꓨwmᗡ0Ǝ⅃0QAUbЯ2qo8ϱƎƨAꞰꞰᗺmϱAta2jЯ⅃QV1მ4jIrᗺYbnH\ᗡjuvZy𝼃ꟼ⅃UMtᗡHOƧxOƧOმƎმUᗡ90MxↄTIƎ9მMr44wv81MlQϽumꓨↄVlQzqਟ8yMਟ81\@7UƧtY9Ƨ##\ИUЯ.ⓄIT ⚪𔗢⚪🞋⚪𔗢⚪ TIⓄ.RUN/##S9YtSU7@/185My85pzQlVcGmuCQlM18vw44rM69EITcxM09DU6E6OSOxSOHDtMULPkyZvujD/HndYBrIj461VQLRj2atAgmBKKAsEg8op2RdUAQ0LE0DmwGa1kWpJaVFeQoG1rX//wMA ⚪𖣠 \\\\
+////\\\\
+////\\\\
+////\\\\
+////\\\\
